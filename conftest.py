529606b06ccc0c0a29c886aa89057d0ff3945c45
V.Nepochatyh <leedno@gmail.com>
2021-08-25 22:33:02 +0300

    Added conftest configuration

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language with command --language")
+
+chrome_cfg = Options()
+chrome_cfg.add_argument('--disable-gpu') 
+chrome_cfg.add_argument('start-maximized')
+
+# Инициализация браузера и сборка мусора
+@pytest.fixture(scope="function")
+def browser(request):
+    print("\nStart browser for case..")
+    if request.config.getoption("language") == None:
+        raise pytest.UsageError("Choose language with command --language")
+    chrome_cfg.add_experimental_option('prefs', {'intl.accept_languages': request.config.getoption("language")})  #Добавляем заголовок языка 
+    browser = webdriver.Chrome(options=chrome_cfg, executable_path=r'C:\chromedriver\chromedriver.exe')
+    yield browser
+    print("\nExit browser for case..")
+    browser.quit()
